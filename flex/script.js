bd1fb1c508f57d93e532d57a5fcb054f911bf023
miskogepa <milovan.jankovic50m@gmail.com>
2025-01-06 17:05:20 +0100

    prvi komit

---
+++ flex/script.js
@@ -0,0 +1,12 @@
+// function pozdrav(){
+//     document.querySelector('.kolona2 .ovde').innertext = 'Pozdrav iz JS-a'}
+
+
+    function pozdrav() {
+        document.querySelector('.kolona2 .ovde').innerText = 'Pozdrav iz JS-a';
+    }
+
+    function jelena(){
+        document.querySelector('.kolona2 .ovde').innerText = 'Jelena';
+    }
+    
---
+++ jeco/script.js
@@ -0,0 +1,10 @@
+function pozdrav(){
+    document.querySelector(".container .p1").innerHTML = "Hello World!";
+}
+function zdravo(){
+    document.querySelector(".container .p2").innerHTML = "Zdravo!";}
+
+    function obrisi(){
+        document.querySelector(".container .p1").innerHTML = "";
+        document.querySelector(".container .p2").innerHTML = "";
+    }
